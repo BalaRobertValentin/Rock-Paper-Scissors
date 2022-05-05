0d1561e768bca37440e87def53a649f32b9e45e9
BalaRobertValentin <balarobertvalentin@gmail.com>
2022-05-05 15:05:50 +0300

    New branch

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+// const container = document.querySelector('#container');
 
-let objects = ["Rock","Paper","Scissors"];
-function computerPlay(objects){
-    return objects[Math.floor(Math.random()*items.length)];
+// const containerp = document.createElement('p');
+// const containerh3 = document.createElement('h3');
+
+// containerp.textContent = 'Hey I’m red!';
+// containerp.style.color = 'red';
+
+// containerh3.textContent = 'I’m a blue h3!';
+// containerh3.style.color = 'blue';
+
+// const containerdiv = document.createElement('div');
+// containerdiv.style.backgroundColor = 'pink';
+// containerdiv.style.border = '1px black';
+
+// const containerdivh1 = document.createElement('h1');
+// const containerdivp = document.createElement('p');
+// containerdivh1.innerText = "I'm in a div";
+// containerdivp.innerText = "ME TOO";
+
+// container.append(containerp,containerh3);
+// containerdiv.append(containerdivh1,containerdivp);
+// container.append(containerdiv);
+const btn = document.querySelector('#btn');
+function alertFunction(){
+    alert ("Oh damn");
 }
-console.log(computerPlay());
+btn.addEventListener('click', alertFunction);
